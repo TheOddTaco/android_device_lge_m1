38955264e3fa865b7081f79fa903e568467ddbc8
Taco <derpytacozlol@gmail.com>
2016-12-18 18:25:53 -0500

    CHANGED KERNEL SOURCE & DISABLED CUSTOM MKBOOTIMG.

--- board/kernel.mk
+++ board/kernel.mk
@@ -3,5 +3,10 @@ BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=m1 user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=soc.0 lpm_levels.sleep_disabled=1 vmalloc=504m
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x80008000 --ramdisk_offset 0x82000000 --tags_offset 0x80000100 
-TARGET_KERNEL_SOURCE := kernel/lge/msm8909
-TARGET_KERNEL_CONFIG := m1_mpcs_us-perf_defconfig
+TARGET_KERNEL_SOURCE := kernel/cyanogen/msm8916
+TARGET_KERNEL_CONFIG := msm8916-perf_defconfig
+
+# Custom boot
+# BOARD_KERNEL_SEPARATED_DT := true
+# BOARD_CUSTOM_BOOTIMG := true
+# BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/boot/mkbootimg.mk
